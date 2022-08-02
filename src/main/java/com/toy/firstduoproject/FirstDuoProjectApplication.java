d55a9e0f3b12b6919802d8dc7f12f40f48522673
jyj2187 <jyjgom@gmail.com>
2022-08-02 19:54:25 +0900

    init

---
+++ src/main/java/com/toy/firstduoproject/FirstDuoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.toy.firstduoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDuoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstDuoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/firstduoproject/FirstDuoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.firstduoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstDuoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
